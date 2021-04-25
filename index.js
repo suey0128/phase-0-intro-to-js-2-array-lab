037d2a463e1396c85d06fedd399704b33e04b971
suey0128 <syui0128@gmail.com>
2021-04-25 16:44:07 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+//const { createSandbox } = require("sinon");
+
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newAppendCat = [...cats,name];
+    return newAppendCat;
+}
+
+function prependCat(name) {
+    const newPrependCat = [name, ...cats];
+    return newPrependCat;
+}
+
+function removeLastCat() {
+    return cats.slice(0,length-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
