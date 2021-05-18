bf511149d3f2c861acfcfce717cc81d1cca3fa6c
suey0128 <syui0128@gmail.com>
2021-05-18 11:46:46 -0700

    commit tree c559bf19f260e7a8bc120ece22758da66aceaac9

--- index.js
+++ index.js
@@ -2,38 +2,90 @@
 
 //const { createSandbox } = require("sinon");
 
-const cats = ["Milo", "Otis", "Garfield"]; 
+// const cats = ["Milo", "Otis", "Garfield"]; 
 
-function destructivelyAppendCat(name) {
-    return cats.push(name);
-}
+// function destructivelyAppendCat(name) {
+//     return cats.push(name);
+// }
 
-function destructivelyPrependCat(name) {
-    return cats.unshift(name);
-}
+// function destructivelyPrependCat(name) {
+//     return cats.unshift(name);
+// }
 
-function destructivelyRemoveLastCat() {
-    return cats.pop();
-}
+// function destructivelyRemoveLastCat() {
+//     return cats.pop();
+// }
 
-function destructivelyRemoveFirstCat() {
-    return cats.shift();
-}
+// function destructivelyRemoveFirstCat() {
+//     return cats.shift();
+// }
+
+// function appendCat(name) {
+//     const newAppendCat = [...cats,name];
+//     return newAppendCat;
+// }
+
+// function prependCat(name) {
+//     const newPrependCat = [name, ...cats];
+//     return newPrependCat;
+// }
+
+// function removeLastCat() {
+//     return cats.slice(0,length-1);
+// }
+
+// function removeFirstCat() {
+//     return cats.slice(1);
+// }
+
+
+
+
+
+
+
+
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+// function destructivelyAppendCat(name) {
+//     cats.push(name);
+// }
+
+let destructivelyAppendCat = name => cats.push(name);
+
+// function destructivelyPrependCat(name) {
+//     cats.unshift(name);
+// } 
+
+let destructivelyPrependCat = name => cats.unshift(name);
+
+// function destructivelyRemoveLastCat() {
+//     cats.pop();
+// }
+
+let destructivelyRemoveLastCat = () => cats.pop();
+
+// function destructivelyRemoveFirstCat() {
+//     cats.shift();
+// }
+
+let destructivelyRemoveFirstCat = () => cats.shift();
 
 function appendCat(name) {
-    const newAppendCat = [...cats,name];
-    return newAppendCat;
+    let newCats = [...cats,name];
+    return newCats;
 }
 
 function prependCat(name) {
-    const newPrependCat = [name, ...cats];
-    return newPrependCat;
+    let newCats = [name, ...cats];
+    return newCats;
 }
 
 function removeLastCat() {
-    return cats.slice(0,length-1);
+    return cats.slice(0,cats.length-1);
 }
 
-function removeFirstCat() {
-    return cats.slice(1);
+function  removeFirstCat() {
+    return cats.slice(1,cats.length);
 }
